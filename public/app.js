7e2f08480a339bd22f4278d7f939262ee01c1fd8
Masafumi Koba <ybiquitous@gmail.com>
2018-11-04 01:47:11 +0900

    Update script

--- public/app.js
+++ public/app.js
@@ -4,6 +4,8 @@ editor.setValue(`rules:
   - id: com.foo.rule1
     pattern: Github
     message: Github -> GitHub
+    fail: Github
+    pass: GitHub
 `);
 
 document.querySelector("#test").addEventListener("click", async () => {
@@ -12,7 +14,7 @@ document.querySelector("#test").addEventListener("click", async () => {
   const response = await fetch("/test", { method: "POST", body });
   switch (response.status) {
     case 200:
-      alert("OK");
+      alert("OK!");
       break;
     case 400:
       const error = await response.text();
